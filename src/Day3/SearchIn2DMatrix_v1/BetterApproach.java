c3f7378b637098492f7d694f8735ba0c1196c975
Sankalp Jadhav <sanky43jadhav@gmail.com>
2022-05-31 08:34:21 +0530

    Better approach by applying Binary search on N rows

---
+++ src/Day3/SearchIn2DMatrix_v1/BetterApproach.java
@@ -0,0 +1,81 @@
+package Day3.SearchIn2DMatrix_v1;
+import java.util.Scanner;
+/*
+
+Important condition given:
+- Every row and every column is sorted in increasing order
+
+Input:
+4
+10 20 30 40
+15 25 35 45
+27 29 37 48
+32 33 39 50
+
+Target: 39
+
+Output: Row: 3 Col: 2
+
+Input:
+4
+10 20 30 40
+15 25 35 45
+27 29 37 48
+32 33 39 50
+
+Target: 28
+
+Output: Not found
+
+Time complexity: O(n * logn) Applying binary search on n rows to find target element
+Space complexity: O(1) Not using extra space
+ */
+public class BetterApproach {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int [][] matrix = new int[n][n];
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                matrix[i][j] = sc.nextInt();
+            }
+        }
+
+        int target = sc.nextInt();
+
+        Pair index = getIndex(matrix, n, target);
+
+        if(index.row==-1 && index.col==-1){
+            System.out.println("Not found");
+        }
+        else{
+            System.out.println("Row: "+index.row+" Col: "+index.col);
+        }
+    }
+
+    private static Pair getIndex(int [][] matrix, int n, int target){
+        Pair index = new Pair(-1, -1);
+
+        for(int i=0;i<n;i++){
+            int low = 0;
+            int high  = matrix[0].length-1;
+
+            while(low<=high){
+                int mid = (low+high)/2;
+                if(matrix[i][mid]==target){
+                    return new Pair(i, mid);
+                }
+                else if(matrix[i][mid]<target){
+                    low = mid+1;
+                }
+                else{ // matrix[i][mid] > target
+                    high = mid-1;
+                }
+            }
+        }
+
+        return index;
+    }
+
+}
