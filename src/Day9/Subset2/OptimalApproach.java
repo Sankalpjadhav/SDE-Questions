41dfcc702680179a06f42588c70e8eb01f6a92db
Sankalp Jadhav <sanky43jadhav@gmail.com>
2022-07-02 18:06:56 +0530

    Brute force approach using recursion and Set

---
+++ src/Day9/Subset2/OptimalApproach.java
@@ -0,0 +1,53 @@
+package Day9.Subset2;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+/*
+Nums array may contain duplicate.
+NOTE: Solution must not have duplicates.
+Input:
+Enter number of elements:
+1
+Enter elements:
+0
+Subsets:
+[][0]
+
+Input:
+Enter number of elements:
+3
+Enter elements:
+1 2 2
+Subsets:
+[1, 2, 2][1][2, 2][2][][1, 2]
+
+Time complexity: O(2^N log 2^N) or O(2^N log 2^N) + O(N)
+O(2^N log 2^N) - Recursion + Adding elements into set
+O(N) - when interviewer asks us to convert it to ArrayList<ArrayList> instead of Set<ArrayList>
+
+Space complexity: O(2^N) : It has 2^N subsets if there are no duplicates.
+ */
+public class OptimalApproach {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of elements: ");
+        int n = sc.nextInt();
+        int [] nums = new int[n];
+        System.out.println("Enter elements: ");
+        for(int i=0;i<n;i++){
+            nums[i] = sc.nextInt();
+        }
+        Set<ArrayList<Integer>> subsets = new HashSet<ArrayList<Integer>>();
+        getSubsets(0, new ArrayList<Integer>(), nums, subsets);
+        System.out.println("Subsets: ");
+        for(ArrayList<Integer> subset:subsets){
+            System.out.print(subset);
+        }
+    }
+
+    private static void getSubsets(int index, ArrayList<Integer> subset, int [] nums, Set<ArrayList<Integer>> subsets){
+
+    }
+}
